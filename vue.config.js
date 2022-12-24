65ed2faaf42082564b853d3b8e1b9f30f555a989
halyapinvv <halyapinvv@yandex.ru>
2022-12-24 20:08:03 +0300

    fix4 host

--- vue.config.js
+++ vue.config.js
@@ -4,29 +4,38 @@
   ],
   devServer: {
     proxy: {
-      'http://localhost:8080': {
+      'http://localhost': {
           target:'https://server-qimw.onrender.com'
       }
-  }
-}
-}*/
-//const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    proxy: 'https://server-qimw.onrender.com'
   },
   configureWebpack: {
     devtool: 'source-map',
   },
 }
+}*/
+
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    host: 'https://front-d1m0.onrender.com',
+    proxy: 'https://server-qimw.onrender.com',
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+  },
+}
+
+
 /*module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
   publicPath: {
     proxy: 'https://server-qimw.onrender.com'
-  }
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+  },
 }*/
